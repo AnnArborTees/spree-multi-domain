b8eb3ad3a6b4a096d148feca7c48af3ac432d564
Nigel Baillie <metreckk@gmail.com>
2014-04-10 14:34:25 -0400

    The home button on the header will now lead to the current store's homepage.

---
+++ app/overrides/decorate_main_nav_bar.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+	virtual_path: "spree/shared/_main_nav_bar",
+	name:         "Adding store homepage link based on curernt store",
+	replace_contents: "li#home-link",
+	text: '<%= link_to Spree.t(:home), "/stores/#{current_store.code}" %>'
+	)
