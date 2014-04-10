1271c73ea5d362e313469be076fb538746bfbbc4
Nigel Baillie <metreckk@gmail.com>
2014-04-10 14:34:00 -0400

    Stores#show no longer redirects
    The url will remain /stores/:code

--- app/controllers/spree/stores_controller.rb
+++ app/controllers/spree/stores_controller.rb
@@ -1,7 +1,18 @@
 class Spree::StoresController < Spree::StoreController
+  helper 'spree/products'
 
   def index
     @stores = Spree::Store.all
+  end
+
+  def show
+    session[:store] = Spree::Store.all.reject { |s| s.code != params[:id] }.first.code
+      
+    @searcher = build_searcher(params)
+    @products = @searcher.retrieve_products
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+
+    render 'spree/home/index'
   end
 
 end
