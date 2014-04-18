248af089344e5f1ddef86d257cb78dc07d909690
Nigel Baillie <metreckk@gmail.com>
2014-04-18 16:05:25 -0400

    Added stores controller spec

--- app/controllers/spree/stores_controller.rb
+++ app/controllers/spree/stores_controller.rb
@@ -15,7 +15,7 @@ class Spree::StoresController < Spree::StoreController
       if current_store.matches_domain? && current_store[0].id == current_store[1].id
         redirect_to current_store[1..-1].full_path(spree.root_path)
       else
-        raise ActiveRecord::RecordNotFound
+        render_404
       end
     elsif current_store.ordered_properly?
       add_current_store_ids_to_params
@@ -24,8 +24,13 @@ class Spree::StoresController < Spree::StoreController
       @taxonomies = Spree::Taxonomy.includes(root: :children)
       render 'spree/home/index'
     else
-      raise ActiveRecord::RecordNotFound
+      render_404
     end
+  end
+
+private
+  def render_404
+    render :file => "#{Rails.root.to_s}/public/404.html",  :status => 404
   end
 
 end
---
+++ spec/controllers/spree/stores_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe Spree::StoresController do
+  
+  describe 'on :show to a valid store' do
+    let!(:store) { FactoryGirl.create(:store) }
+
+    it 'should return 200' do
+      spree_get :show, store_codes: store.code
+      expect(response.response_code).to eq 200
+    end
+  end
+
+  describe 'on :show with 2 mutually populated stores' do
+    let!(:store1) { FactoryGirl.create(:store,
+            name: 'Store 1',
+            code: 'store1',
+            url_index: '0') }
+    let!(:store2) { FactoryGirl.create(:store,
+            name: 'Store 2',
+            code: 'store2',
+            url_index: '1') }
+
+    let!(:product1) { FactoryGirl.create(:product,
+            name: 'Product 1',
+            slug: 'product1',
+            stores: [store1, store2]) }
+    let!(:product2) { FactoryGirl.create(:product,
+            name: 'Product 2',
+            slug: 'product2',
+            stores: [store1, store2]) }
+
+    it 'mixed in correct order, should return 200' do
+      spree_get :show, store_codes: 'store1/store2'
+      expect(response.response_code).to eq 200
+    end
+
+    it 'mixed out of order, should return 404' do
+      spree_get :show, store_codes: 'store2/store1'
+      expect(response.response_code).to eq 404
+    end
+  end
+
+end
