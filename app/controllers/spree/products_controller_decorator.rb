807360f82638e15624f12a00963cabf2fef33e99
Nigel Baillie <metreckk@gmail.com>
2014-04-22 11:45:25 -0400

    Fixed respond_to? 'contains_any_of' to be respond_to? 'contains_any_of?'

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -6,9 +6,9 @@ Spree::ProductsController.class_eval do
 
   def can_show_product
     @product ||= Spree::Product.friendly.find(params[:id])
-    if @product.stores.empty? || 
-    	( current_store.respond_to?('contains_any_of') ? 
-    		!current_store.contains_any_of?(@product.stores) : !@product.stores.include?(current_store) )
+    if @product.stores.empty? || ( current_store.respond_to?('contains_any_of?') ? 
+    		                            !current_store.contains_any_of?(@product.stores) : 
+                                    !@product.stores.include?(current_store) )
       raise ActiveRecord::RecordNotFound
     end
   end
