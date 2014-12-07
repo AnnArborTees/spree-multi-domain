8afac6800c2c3c65f775246c270f058985570ef1
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-12-07 00:14:50 -0500

    Added index to taxonomies_stores

---
+++ db/migrate/20141207051050_add_indexes_stores_taxonomies.rb
@@ -0,0 +1,6 @@
+class AddIndexesStoresTaxonomies < ActiveRecord::Migration
+  def change
+    add_index :spree_taxonomies_stores, :store_id
+    add_index :spree_taxonomies_stores, :taxonomy_id
+  end
+end
